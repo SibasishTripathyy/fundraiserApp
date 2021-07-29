779d4ba2af22dee9f9db261154954a2f3e228357
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2021-07-29 19:29:48 +0530

    database works now

--- src/main/java/com/ngo/fundraiser/controller/ThemeController.java
+++ src/main/java/com/ngo/fundraiser/controller/ThemeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("theme")
+@RequestMapping("/theme")
 public class ThemeController {
 
     @Autowired
